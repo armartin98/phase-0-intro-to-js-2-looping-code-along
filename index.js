2d8ec5374ec3be5dad015fb76526c11cb683399a
Andrew <armartin1998@gmail.com>
2023-02-27 17:34:41 -0500

    Complete looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let message = []
+    for (let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message
+}
+
+function countDown(number){
+    let i = number
+    while (i >= 0){
+        console.log(i)
+        i--
+    }
+}
